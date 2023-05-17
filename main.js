89cc1b7116541a8dae7e99590ac914f4d19c1f9b
grey123son <94587086+grey123son@users.noreply.github.com>
2023-05-17 16:40:22 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,24 +1,28 @@
 function preload(){
 }
-
 function setup(){
-    canvas = createCanvas(640, 480);
-    canvas.position(600, 280);
-    video = createCapture(VIDEO);
-    video.hide();
+canvas = createCanvas(300, 300);
+canvas.center();
+video = createCapture(VIDEO);
+video.size(300,300);
+video.hide();
 
-    tint_color = "";
+poseNet = ml5.poseNet(video, modelLoaded);
+poseNet.on('pose', gotPoses);
 }
-
 function draw(){
-    image(video, 0,0,640,480);
-    tint(tint_color);
+    image(video, 0, 0, 300, 300);
 }
-
 function take_snapshot(){
-    save("tinted_screenshot.png");
+    save("filter_snapshot.png");
 }
-
-function filter_tint(){
-    tint_color = document.getElementById("tan").value;
+function modelLoaded(){
+    console.log("Pose Net has been initialized")
+}
+function gotPoses(results){
+    if(results.length > 0){
+    console.log(results);
+    console.log("nose x = " + results[0].pose.nose.x);
+    console.log("nose y = " + results[0].pose.nose.y);
+    }
 }
